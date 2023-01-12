f58f09ba0e233be03215e6640325c469e1b56dff
jgrisal6 <grisalesjuanpablo18@gmail.com>
2023-01-11 23:34:14 -0500

    Creación del proyecto en Angular,creación de páginas: Home, register, menu y game. Se eliminan archivo HTML y CSS de la primera versión del prototipo en CSS y HTML Sprint 4

---
+++ coinobille-project/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { GameComponent } from './pages/game/game.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    MenuComponent,
+    GameComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
