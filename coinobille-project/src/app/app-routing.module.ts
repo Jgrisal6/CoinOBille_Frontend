8274e309a31414f9684ec99249cc42ab3d29148c
jgrisal6 <grisalesjuanpablo18@gmail.com>
2023-01-12 00:23:55 -0500

    Se actualizan las rutas de renderizado, el primer routerlink en el home

--- coinobille-project/src/app/app-routing.module.ts
+++ coinobille-project/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { GameComponent } from './pages/game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'menu',
+    component: MenuComponent,
+  },
+  {
+    path: 'game',
+    component: GameComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
